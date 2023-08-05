d6549386335a7d2401efdd6f2fbbd285e8c4cbcc
adenlall <adenlall@outlook.sa>
2023-08-05 15:23:25 +0100

    UI fixes

--- src/components/AdblockCase/selectByUser.js
+++ src/components/AdblockCase/selectByUser.js
@@ -147,12 +147,19 @@ function SelectByUser() {
 
     return (
 		<>
-			<div className="lg:w-[40%] w-full md:h-auto shadow-xl flex flex-col justify-between items-stretch p-4 rounded-lg bg-slate-100 dark:bg-gray-800 dark:text-slate-100 text-gray-800">
+			<div className="lg:w-[40%] lg:h-[38em] lg:max-h-[80vh] h-auto w-full md:h-auto shadow-xl flex flex-col justify-between items-stretch p-4 rounded-lg bg-slate-100 dark:bg-gray-800 dark:text-slate-100 text-gray-800">
 				<div className="flex flex-col h-full w-full justify-evenly items-center">
 					<div className="flex flex-row items-end w-[87%] py-6 space-x-2">
 						<header className="text-2xl lg:text-4xl font-bold">Find your Location.</header>
+						<div className="tooltip tooltip-open tooltip-warning" data-tip="we recommend you select your actual location, to get real times because the location and your local time should be synchronize.">
+							<button className="btn btn-square btn-sm">
+								<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M12 15H12.01M12 12V9M4.98207 19H19.0179C20.5615 19 21.5233 17.3256 20.7455 15.9923L13.7276 3.96153C12.9558 2.63852 11.0442 2.63852 10.2724 3.96153L3.25452 15.9923C2.47675 17.3256 3.43849 19 4.98207 19Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+								</svg>
+							</button>
+						</div>
 					</div>
-					<input onChange={hundelSearch} type="text" placeholder="Search with Nomination API" className="input input-bordered text-black input-primary w-full max-w-xs" />
+					<input onChange={hundelSearch} type="text" placeholder="Search with Nomination API" className="input input-bordered text-black input-primary w-[87%] max-w-[87] my-6" />
 					<div className="max-h-[16em] my-10 w-full h-full overflow-scroll">
 						<div className="join join-vertical w-full">
 							{dataUI}
